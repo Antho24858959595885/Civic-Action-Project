a061e78caac6f09daef6bb03cafbbca0d9564c5a
Dylan Carter <167344346+dylcarinc@users.noreply.github.com>
2026-04-20 12:06:49 -0500

    fixed project page issue

--- CivicAction/obj/Debug/net10.0/CivicAction.AssemblyInfo.cs
+++ CivicAction/obj/Debug/net10.0/CivicAction.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CivicAction")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0a2da8f9d381696a643bbf06f3be0d2fddf9306")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed2fd74e7faa3a026c0ee048cbb946e4798ba241")]
 [assembly: System.Reflection.AssemblyProductAttribute("CivicAction")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CivicAction")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
